7537ff31b6628079200780492f6e90768ba24692
limoza <svacc.nelllely@gmail.com>
2023-10-25 23:11:28 +0900

    Jest導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { test } from "./styles/test.css";
+import { Ex } from "./Components/Ex";
 
 export default function Home() {
   return (
     <main>
       <div className={test}>something</div>
+      <Ex />
     </main>
   );
 }
